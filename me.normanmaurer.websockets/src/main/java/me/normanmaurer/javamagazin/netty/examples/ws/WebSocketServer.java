5c64cd826b306c3a5857a4390fb9b00ebc374325
tfreese <commercial@freese-home.de>
2012-10-16 19:00:54 +0200

    added: shutdown for websocket-server
    Signed-off-by: tfreese <commercial@freese-home.de>

--- me.normanmaurer.websockets/src/main/java/me/normanmaurer/javamagazin/netty/examples/ws/WebSocketServer.java
+++ me.normanmaurer.websockets/src/main/java/me/normanmaurer/javamagazin/netty/examples/ws/WebSocketServer.java
@@ -4,7 +4,9 @@ import java.net.InetSocketAddress;
 
 import org.jboss.netty.bootstrap.ConnectionlessBootstrap;
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.ChannelGroupFuture;
 import org.jboss.netty.channel.group.DefaultChannelGroup;
 import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
@@ -72,25 +74,60 @@ public class WebSocketServer
 	 */
 	public void startUp()
 	{
+		final ChannelGroup allChannels = new DefaultChannelGroup("websocket-server");
+
 		// Bereite den UDP/Datagram Channel vor
-		ConnectionlessBootstrap udpBootstrap =
+		final ConnectionlessBootstrap udpBootstrap =
 				new ConnectionlessBootstrap(new NioDatagramChannelFactory());
+		udpBootstrap.setOption("reuseAddress", Boolean.TRUE);
 
 		// Setzen der WebSocketBroadcastPipelineFactory die das Senden von UDP
 		// Nachrichten an die WebSocket Clients uebernimmt
 		udpBootstrap.setPipelineFactory(new WebSocketBroadcastPipelineFactory(this.group));
 
 		// Binden des Sockets der die UDP Nachrichten entgegennimmt
-		udpBootstrap.bind(new InetSocketAddress(this.udpPort));
+		Channel channel = udpBootstrap.bind(new InetSocketAddress(this.udpPort));
+		allChannels.add(channel);
 
 		// Bereite den Channel vor
-		ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory());
+		final ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory());
+		bootstrap.setOption("reuseAddress", Boolean.TRUE);
 
 		// Setzen der WebSocketPipelineFactory, die das Bearbeiten von HTTP
 		// und WebSocket Requests uebernimmt
 		bootstrap.setPipelineFactory(new WebSocketPipelineFactory(this.group));
 
 		// Binden des Sockets der nun bereit ist Requests entgegen zu nehmen
-		bootstrap.bind(new InetSocketAddress(this.port));
+		channel = bootstrap.bind(new InetSocketAddress(this.port));
+		allChannels.add(channel);
+
+		// Stoppen
+		Runtime.getRuntime().addShutdownHook(new Thread()
+		{
+			/**
+			 * @see java.lang.Thread#run()
+			 */
+			@Override
+			public void run()
+			{
+				System.out.println("shutdown");
+
+				ChannelGroupFuture future = allChannels.close();
+				future.awaitUninterruptibly();
+				udpBootstrap.releaseExternalResources();
+				bootstrap.releaseExternalResources();
+
+				// Client stoppen
+				// ChannelFactory factory = ...;
+				// ClientBootstrap bootstrap = ...;
+				// ChannelFuture future = bootstrap.connect(...);
+				// future.awaitUninterruptibly();
+				// if (!future.isSuccess()) {
+				// future.getCause().printStackTrace();
+				// }
+				// future.getChannel().getCloseFuture().awaitUninterruptibly();
+				// factory.releaseExternalResources();
+			}
+		});
 	}
 }
