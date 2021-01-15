46fd23eacd09c2f4650e1976da02a160db1d0b26
tfreese <commercial@freese-home.de>
2021-01-15 18:01:59 +0100

    polish

--- misc-cache/src/main/java/de/freese/cache/Hazelcast_Node2.java
+++ misc-cache/src/main/java/de/freese/cache/Hazelcast_Node2.java
@@ -9,7 +9,7 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+import com.hazelcast.map.IMap;
 
 /**
  * @author Thomas Freese
